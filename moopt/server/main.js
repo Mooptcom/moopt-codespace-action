cb197bf7463003cfc76d2cba2e4eb5533754dfd8
Kai Feng Chew <kafechew@gmail.com>
2023-05-19 18:50:44 +0800

    successfully use JohnnyFive to command arduino (blinking builtin LED)

--- moopt/server/main.js
+++ moopt/server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+//import Cylon from 'cylon';
+import JohnnyFive from 'johnny-five';
+
+var five = require("johnny-five"),
+    board = new five.Board({ port : '/dev/cu.wchusbserial1410' });
 
 Meteor.startup(() => {
   // code to run on server at startup
+  board.on('error', function (error) {
+      console.error('Johnny Five Error', error);
+  });
+
+  board.on("ready", Meteor.bindEnvironment(function() {
+
+        // The Arduino UNO is equipped with some special pins, like pin 13 (top right), is connected with the builtin LED. 
+        // The builtin LED is marked L on the PCB.
+        var led = new five.Led(13);
+
+        //
+        led.blink(1000);
+
+    }));
 });
