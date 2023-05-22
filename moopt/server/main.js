869ddaea5a1ac776920c8c4db33f2c9d423ab89d
Kai Feng Chew <kafechew@gmail.com>
2023-05-22 13:51:06 +0800

    clean up meteor packages

--- moopt/server/main.js
+++ moopt/server/main.js
@@ -2,21 +2,21 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
 
-/*import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import JohnnyFive from 'johnny-five';
 
 var five = require("johnny-five");
 const { Board, Thermometer } = require("johnny-five");
-const board = new Board({ port : '/dev/cu.wchusbserial1410' });*/
+const board = new Board({ port : '/dev/cu.wchusbserial1410' });
 
 
 Meteor.startup(() => {
   // code to run on server at startup
-  /*board.on('error', function (error) {
+  board.on('error', function (error) {
       console.error('Johnny Five Error', error);
-  });*/
+  });
 
-  //board.on("ready", Meteor.bindEnvironment(function() {
+  board.on("ready", Meteor.bindEnvironment(function() {
       /*// The Arduino UNO is equipped with some special pins, like pin 13 (top right), is connected with the builtin LED. 
       // The builtin LED is marked L on the PCB.
       var led = new five.Led(13);
@@ -25,7 +25,7 @@ Meteor.startup(() => {
 
       // This requires OneWire support using ConfigurableFirmata
       // https://github.com/firmata/ConfigurableFirmata & http://firmatabuilder.com/ 
-      /*const thermometer = new Thermometer({
+      const thermometer = new Thermometer({
         controller: "DS18B20",
         pin: 5
       });
@@ -37,6 +37,6 @@ Meteor.startup(() => {
         console.log("  fahrenheit   : ", fahrenheit);
         console.log("  kelvin       : ", kelvin);
         console.log("--------------------------------------");
-      });*/
-  //}));
+      });
+  }));
 });
